c0d3c2917a106b81ce0ea2df195f1226a6af9563
Shalom <tjoelshalom@gmail.com>
2024-10-05 16:37:06 -0600

    agregando component-login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BannerComponent } from './components/banner/banner/banner.component';
+import {ComponentLoginComponent} from './components/component-login/component-login.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BannerComponent],
+  imports: [RouterOutlet, BannerComponent, ComponentLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
